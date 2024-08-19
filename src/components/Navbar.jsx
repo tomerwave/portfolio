719d45b540036ccafb5cffb3f9cdbedfa97682da
tomerwave <tomergal1404@gmail.com>
2024-08-19 22:54:51 +0300

    Changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-            TomerWave
+            TomerWave &nbsp;
+            <span className="sm:block hidden"> | Software Mastery</span>
           </p>
         </Link>
 
